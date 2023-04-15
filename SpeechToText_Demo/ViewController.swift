468890eb16dcad56805492533b7955eeec826854
le.khoi <le.khoi@sun-asterisk.com>
2023-04-15 18:31:02 +0700

    Initial Commit

---
+++ SpeechToText_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeechToText_Demo
+//
+//  Created by KhoiLe on 15/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
